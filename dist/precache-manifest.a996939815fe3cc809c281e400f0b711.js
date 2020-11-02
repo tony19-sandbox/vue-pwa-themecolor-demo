3d2998773085b9ac44e9dc67266d49848097236a
Tony Trinh <tony19@gmail.com>
2020-11-02 15:52:25 -0600

    Add dist

---
+++ dist/precache-manifest.a996939815fe3cc809c281e400f0b711.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d63c6a7ff10c74e76e54",
+    "url": "/css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "8870e99f8b364f6a4b1312836a5787ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d63c6a7ff10c74e76e54",
+    "url": "/js/app.613880f8.js"
+  },
+  {
+    "revision": "9d752fd5dee9ef7e5092",
+    "url": "/js/chunk-vendors.7b5ac5b3.js"
+  },
+  {
+    "revision": "78ad72f12096343688657f7e21d1e4d4",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
